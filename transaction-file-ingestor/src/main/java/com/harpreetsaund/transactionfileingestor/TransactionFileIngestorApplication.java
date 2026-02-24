8e592c3bd3aff9c971cf0270090599abf279b5a8
Harpreet Saund <saundharpreet99@gmail.com>
2026-02-24 13:35:33 -0500

    file to kafka project skeleton

---
+++ transaction-file-ingestor/src/main/java/com/harpreetsaund/transactionfileingestor/TransactionFileIngestorApplication.java
@@ -0,0 +1,13 @@
+package com.harpreetsaund.transactionfileingestor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionFileIngestorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransactionFileIngestorApplication.class, args);
+    }
+
+}
---
+++ transaction-file-ingestor/src/test/java/com/harpreetsaund/transactionfileingestor/TransactionFileIngestorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harpreetsaund.transactionfileingestor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionFileIngestorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
